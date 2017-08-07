707a39e2a04874d020c18265c94cdbf8fb88bf00
Pedro Vitti <pedro@autobutler.de>
2017-08-07 08:59:42 +0200

    Add rest to get games information.

---
+++ flask_rest_service/resources.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+from flask import request, abort
+from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from flask_rest_service import app, api, mongo
+from bson.objectid import ObjectId
+
+class GameList(Resource):
+    def get(self):
+        return [x for x in mongo.db.games.find()]
+
+
+class Game(Resource):
+    def get(self, game_id):
+        return mongo.db.games.find_one_or_404({"_id": game_id})
+
+
+class Root(Resource):
+    def get(self):
+        return {
+            'status': 'OK',
+            'mongo': mongo.db,
+        }
+
+api.add_resource(Root, '/')
+api.add_resource(GameList, '/games/')
+api.add_resource(Game, '/games/<ObjectId:game_id>')
